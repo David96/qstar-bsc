0240388c2946970033f915fa94ddcda99268ddd4
David96 <david@hameipe.de>
2019-05-13 14:26:07 +0200

    Cleanup a bit and only use our own qstarHists class

--- src/qstarHists.cxx
+++ src/qstarHists.cxx
@@ -1,5 +1,6 @@
 #include "UHH2/qstar/include/qstarHists.h"
 #include "UHH2/core/include/Event.h"
+#include "UHH2/common/include/Utils.h"
 
 #include "TH1F.h"
 #include "TH2F.h"
@@ -12,27 +13,36 @@ using namespace uhh2examples;
 qstarHists::qstarHists(Context & ctx, const string & dirname): Hists(ctx, dirname){
   // book all histograms here
   // jets
-  book<TH1F>("N_jets", "N_{jets}", 20, 0, 20);  
-  book<TH1F>("N_PU", "N_{PU}", 100, 0, 100);  
+  book<TH1F>("N_jets", "N_{jets}", 20, 0, 20);
+  book<TH1F>("N_PU", "N_{PU}", 100, 0, 100);
   book<TH1F>("eta_jet1", "#eta^{jet 1}", 40, -2.5, 2.5);
   book<TH1F>("pt_jet1", "p_{T}^{jet 1}", 100, 10, 500);
   book<TH1F>("eta_jet2", "#eta^{jet 2}", 40, -2.5, 2.5);
   book<TH1F>("eta_jet3", "#eta^{jet 3}", 40, -2.5, 2.5);
   book<TH1F>("eta_jet4", "#eta^{jet 4}", 40, -2.5, 2.5);
 
-  book<TH1F>("EMcharged_jet1", "EMcharged_jet1", 100,0.0,1.0);
-  book<TH1F>("EMneutral_jet1", "EMneutral_jet1", 100,0.0,1.0);
+  //substructure
+  book<TH1F>("SoftDropMass_1", "M_{1}^{SD} [GeV/c^{2}]", 50,0,300);
+  book<TH1F>("CHF_1","CHF_{1}",100,0,1);
+  book<TH1F>("TAU1_1","#tau_{1_{1}}",20,0,1);
+  book<TH1F>("TAU2_1","#tau_{2_{1}}",20,0,1);
+  book<TH1F>("Tau21_1", "#tau_{2_{1}}/#tau_{1_{1}}", 20,0,1);
+
+  book<TH1F>("SoftDropMass_2", "M_{2}^{SD} [GeV/c^{2}]", 50,0,300);
+  book<TH1F>("CHF_2","CHF_{2}",100,0,1);
+  book<TH1F>("TAU1_2","#tau_{1_{2}}",20,0,1);
+  book<TH1F>("TAU2_2","#tau_{2_{2}}",20,0,1);
+  book<TH1F>("Tau21_2", "#tau_{2_{2}}/#tau_{1_{2}}", 20,0,1);
+
+  book<TH1F>("invMass","M_{jj}-AK8 [GeV/c^{2}]",30,1000,7000);
+
   book<TH1F>("HADcharged_jet1", "HADcharged_jet1", 100,0.0,1.0);
   book<TH1F>("HADneutral_jet1", "HADneutral_jet1", 100,0.0,1.0);
 
-  book<TH2D>("EMcharged_vs_eta_jet1","EMcharged vs #eta; #eta; EMcharged",100,-6,6,100,0.0,1.0);   
-  book<TH2D>("EMneutral_vs_eta_jet1","EMneutral vs #eta; #eta; EMneutral",100,-6,6,100,0.0,1.0);   
-  book<TH2D>("HADcharged_vs_eta_jet1","HADcharged vs #eta; #eta; HADcharged",100,-6,6,100,0.0,1.0);   
-  book<TH2D>("HADneutral_vs_eta_jet1","HADneutral vs #eta; #eta; HADneutral",100,-6,6,100,0.0,1.0);   
-  book<TH2D>("EMcharged_vs_PU_jet1","EMcharged vs PU; PU; EMcharged",100,0,100,100,0.0,1.0);   
-  book<TH2D>("EMneutral_vs_PU_jet1","EMneutral vs PU; PU; EMneutral",100,0,100,100,0.0,1.0);   
-  book<TH2D>("HADcharged_vs_PU_jet1","HADcharged vs PU; PU; HADcharged",100,0,100,100,0.0,1.0);   
-  book<TH2D>("HADneutral_vs_PU_jet1","HADneutral vs PU; PU; HADneutral",100,0,100,100,0.0,1.0);   
+  book<TH2D>("HADcharged_vs_eta_jet1","HADcharged vs #eta; #eta; HADcharged",100,-6,6,100,0.0,1.0);
+  book<TH2D>("HADneutral_vs_eta_jet1","HADneutral vs #eta; #eta; HADneutral",100,-6,6,100,0.0,1.0);
+  book<TH2D>("HADcharged_vs_PU_jet1","HADcharged vs PU; PU; HADcharged",100,0,100,100,0.0,1.0);
+  book<TH2D>("HADneutral_vs_PU_jet1","HADneutral vs PU; PU; HADneutral",100,0,100,100,0.0,1.0);
 
 
   // leptons
@@ -51,43 +61,78 @@ void qstarHists::fill(const Event & event){
   // 'hist' is used here a lot for simplicity, but it will be rather
   // slow when you have many histograms; therefore, better
   // use histogram pointers as members as in 'UHH2/common/include/ElectronHists.h'
-  
+
   // Don't forget to always use the weight when filling.
   double weight = event.weight;
-  
-  std::vector<Jet>* jets = event.jets;
-  int Njets = jets->size();
+
+  std::vector<TopJet> jets = *event.topjets;
+  int Njets = jets.size();
   hist("N_jets")->Fill(Njets, weight);
   if(!event.isRealData)  hist("N_PU")->Fill(event.genInfo->pileup_TrueNumInteractions(), weight);
 
   if(Njets>=1){
-    hist("eta_jet1")->Fill(jets->at(0).eta(), weight);
-    hist("pt_jet1")->Fill(jets->at(0).pt(), weight);
-    hist("EMcharged_jet1")->Fill(jets->at(0).chargedEmEnergyFraction(), weight);
-    hist("EMneutral_jet1")->Fill(jets->at(0).neutralEmEnergyFraction(), weight);
-    hist("HADcharged_jet1")->Fill(jets->at(0).chargedHadronEnergyFraction(), weight);
-    hist("HADneutral_jet1")->Fill(jets->at(0).neutralHadronEnergyFraction(), weight);
-    
-    ((TH2D*)hist("EMcharged_vs_eta_jet1"))->Fill(jets->at(0).eta(),jets->at(0).chargedEmEnergyFraction(), weight);
-    ((TH2D*)hist("EMneutral_vs_eta_jet1"))->Fill(jets->at(0).eta(),jets->at(0).neutralEmEnergyFraction(), weight);
-    ((TH2D*)hist("HADcharged_vs_eta_jet1"))->Fill(jets->at(0).eta(),jets->at(0).chargedHadronEnergyFraction(), weight);
-    ((TH2D*)hist("HADneutral_vs_eta_jet1"))->Fill(jets->at(0).eta(),jets->at(0).neutralHadronEnergyFraction(), weight);
+    hist("eta_jet1")->Fill(jets[0].eta(), weight);
+    hist("pt_jet1")->Fill(jets[0].pt(), weight);
+    hist("HADcharged_jet1")->Fill(jets[0].chargedHadronEnergyFraction(), weight);
+    hist("HADneutral_jet1")->Fill(jets[0].neutralHadronEnergyFraction(), weight);
+
+    ((TH2D*)hist("HADcharged_vs_eta_jet1"))->Fill(jets[0].eta(),jets[0].chargedHadronEnergyFraction(), weight);
+    ((TH2D*)hist("HADneutral_vs_eta_jet1"))->Fill(jets[0].eta(),jets[0].neutralHadronEnergyFraction(), weight);
     if(!event.isRealData){
-      ((TH2D*)hist("EMcharged_vs_PU_jet1"))->Fill(event.genInfo->pileup_TrueNumInteractions(),jets->at(0).chargedEmEnergyFraction(), weight);
-      ((TH2D*)hist("EMneutral_vs_PU_jet1"))->Fill(event.genInfo->pileup_TrueNumInteractions(),jets->at(0).neutralEmEnergyFraction(), weight);
-      ((TH2D*)hist("HADcharged_vs_PU_jet1"))->Fill(event.genInfo->pileup_TrueNumInteractions(),jets->at(0).chargedHadronEnergyFraction(), weight);
-      ((TH2D*)hist("HADneutral_vs_PU_jet1"))->Fill(event.genInfo->pileup_TrueNumInteractions(),jets->at(0).neutralHadronEnergyFraction(), weight);
+      ((TH2D*)hist("HADcharged_vs_PU_jet1"))->Fill(event.genInfo->pileup_TrueNumInteractions(),jets[0].chargedHadronEnergyFraction(), weight);
+      ((TH2D*)hist("HADneutral_vs_PU_jet1"))->Fill(event.genInfo->pileup_TrueNumInteractions(),jets[0].neutralHadronEnergyFraction(), weight);
     }
   }
   if(Njets>=2){
-    hist("eta_jet2")->Fill(jets->at(1).eta(), weight);
+    hist("eta_jet2")->Fill(jets[1].eta(), weight);
   }
   if(Njets>=3){
-    hist("eta_jet3")->Fill(jets->at(2).eta(), weight);
+    hist("eta_jet3")->Fill(jets[2].eta(), weight);
   }
   if(Njets>=4){
-    hist("eta_jet4")->Fill(jets->at(3).eta(), weight);
+    hist("eta_jet4")->Fill(jets[3].eta(), weight);
   }
+
+  // Substructure
+  if(jets.size()<1) return;
+
+  if (jets.size() >= 2) {
+    const auto & jet1 = jets[0];
+    const auto & jet2 = jets[1];
+    LorentzVector subjet_sum1;
+    LorentzVector subjet_sum2;
+    for (const auto s1 : jet1.subjets()) {
+      subjet_sum1 += s1.v4();
+    }
+    auto JetSDMass1 = subjet_sum1.M();
+    for (const auto s2 : jet2.subjets()) {
+      subjet_sum2 += s2.v4();
+    }
+    auto JetSDMass2 = subjet_sum2.M();
+    hist("SoftDropMass_1")->Fill(JetSDMass1, weight);
+    hist("SoftDropMass_2")->Fill(JetSDMass2, weight);
+  }
+  float chf_1 = jets[0].chargedHadronEnergyFraction();
+  hist("CHF_1")->Fill(chf_1, weight);
+  float tau1_1 = jets[0].tau1();
+  hist("TAU1_1")->Fill(tau1_1, weight);
+  float tau2_1 = jets[0].tau2();
+  hist("TAU2_1")->Fill(tau2_1, weight);
+  float tau21_1 = tau2_1/tau1_1;
+  hist("Tau21_1")->Fill(tau21_1, weight);
+
+  float chf_2 = jets[1].chargedHadronEnergyFraction();
+  hist("CHF_2")->Fill(chf_2, weight);
+  float tau1_2 = jets[1].tau1();
+  hist("TAU1_2")->Fill(tau1_2, weight);
+  float tau2_2 = jets[1].tau2();
+  hist("TAU2_2")->Fill(tau2_2, weight);
+  float tau21_2 = tau2_2/tau1_2;
+  hist("Tau21_2")->Fill(tau21_2, weight);
+
+  //      float mass = (jets[0].v4() + jets[1].v4()).M();
+  float mass = inv_mass_safe(jets[0].v4() + jets[1].v4());
+  hist("invMass")->Fill(mass, weight);
 
   int Nmuons = event.muons->size();
   hist("N_mu")->Fill(Nmuons, weight);
@@ -96,7 +141,7 @@ void qstarHists::fill(const Event & event){
       hist("eta_mu")->Fill(thismu.eta(), weight);
       hist("reliso_mu")->Fill(thismu.relIso(), weight);
   }
-  
+
   int Npvs = event.pvs->size();
   hist("N_pv")->Fill(Npvs, weight);
 }
--- src/qstarModule.cxx
+++ src/qstarModule.cxx
@@ -9,6 +9,7 @@
 #include "UHH2/common/include/NSelections.h"
 #include "UHH2/qstar/include/qstarSelections.h"
 #include "UHH2/qstar/include/qstarHists.h"
+#include "UHH2/common/include/JetHists.h"
 
 using namespace std;
 using namespace uhh2;
@@ -31,13 +32,14 @@ private:
     std::unique_ptr<CommonModules> common;
     
     std::unique_ptr<JetCleaner> jetcleaner;
+    std::unique_ptr<TopJetCleaner> topJetCleaner;
    
     // declare the Selections to use. Use unique_ptr to ensure automatic call of delete in the destructor,
     // to avoid memory leaks.
     std::unique_ptr<Selection> njet_sel, dijet_sel;
     
     // store the Hists collection as member variables. Again, use unique_ptr to avoid memory leaks.
-    std::unique_ptr<Hists> h_nocuts, h_njet, h_dijet, h_ele;
+    std::unique_ptr<Hists> h_nocuts, h_cleaner, h_njet, h_dijet;
 };
 
 
@@ -46,33 +48,32 @@ qstarModule::qstarModule(Context & ctx){
     // member variables, in particular the AnalysisModules such as
     // CommonModules or some cleaner module, Selections and Hists.
     // But you can do more and e.g. access the configuration, as shown below.
-    
-    cout << "Hello World from qstarModule!" << endl;
-    
+
     // If needed, access the configuration of the module here, e.g.:
-    string testvalue = ctx.get("TestKey", "<not set>");
-    cout << "TestKey in the configuration was: " << testvalue << endl;
-    
+    // string testvalue = ctx.get("TestKey", "<not set>");
+    // cout << "TestKey in the configuration was: " << testvalue << endl;
+
     // If running in SFrame, the keys "dataset_version", "dataset_type", "dataset_lumi",
     // and "target_lumi" are set to the according values in the xml file. For CMSSW, these are
     // not set automatically, but can be set in the python config file.
     for(auto & kv : ctx.get_all()){
         cout << " " << kv.first << " = " << kv.second << endl;
     }
-    
+
     // 1. setup other modules. CommonModules and the JetCleaner:
     common.reset(new CommonModules());
     // TODO: configure common here, e.g. by 
     // calling common->set_*_id or common->disable_*
     common->init(ctx);
-    jetcleaner.reset(new JetCleaner(ctx, 30.0, 2.4)); 
-    
+    jetcleaner.reset(new JetCleaner(ctx, 30.0, 2.4));
+    topJetCleaner.reset(new TopJetCleaner(ctx, PtEtaCut(200., 2.4)));
+
     // note that the JetCleaner is only kept for the sake of example;
     // instead of constructing a jetcleaner explicitly,
     // the cleaning can also be achieved with less code via CommonModules with:
     // common->set_jet_id(PtEtaCut(30.0, 2.4));
     // before the 'common->init(ctx)' line.
-    
+
     // 2. set up selections
     njet_sel.reset(new NJetSelection(2)); // see common/include/NSelections.h
     dijet_sel.reset(new DijetSelection()); // see qstarSelections
@@ -81,7 +82,7 @@ qstarModule::qstarModule(Context & ctx){
     h_nocuts.reset(new qstarHists(ctx, "NoCuts"));
     h_njet.reset(new qstarHists(ctx, "Njet"));
     h_dijet.reset(new qstarHists(ctx, "Dijet"));
-    h_ele.reset(new ElectronHists(ctx, "ele_nocuts"));
+    h_cleaner.reset(new qstarHists(ctx, "Cleaner"));
 }
 
 
@@ -95,17 +96,22 @@ bool qstarModule::process(Event & event) {
     // this is controlled by the return value of this method: If it
     // returns true, the event is kept; if it returns false, the event
     // is thrown away.
-    
-    cout << "qstarModule: Starting to process event (runid, eventid) = (" << event.run << ", " << event.event << "); weight = " << event.weight << endl;
-    
+
+#ifdef DEBUG
+    cout << "qstarModule: Starting to process event (runid, eventid) = ("
+         << event.run << ", " << event.event << "); weight = " << event.weight << endl;
+#endif
+
+    h_nocuts->fill(event);
+
     // 1. run all modules other modules.
     common->process(event);
     jetcleaner->process(event);
-    
+    topJetCleaner->process(event);
+
+    h_cleaner->fill(event);
+
     // 2. test selections and fill histograms
-    h_ele->fill(event);
-    h_nocuts->fill(event);
-    
     bool njet_selection = njet_sel->passes(event);
     if(njet_selection){
         h_njet->fill(event);
